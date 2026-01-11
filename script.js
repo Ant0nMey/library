d00b4d050633ffa6f578869456ec4a371336ac81
Ant0n <root@UOP-0040-INFO.challauch.local>
2026-01-11 12:55:27 +0100

    manque modification checkbox dans objet livre

--- script.js
+++ script.js
@@ -25,36 +25,95 @@ function formData() {
   }
 }
 
-function creationDivCarte (className, nomLabel, bookPropertie) {
-  const titre = document.createElement("div");
-  titre.className = className;
-    const titreLabel = document.createElement("h1");
-    titreLabel.textContent = nomLabel;
-    const titreInput = document.createElement("div")
-    titreInput.className = "carteInput";
-    titreInput.textContent = bookPropertie;
-  titre.appendChild(titreLabel);
-  titre.appendChild(titreInput);
-  return titre;
+function creerSectionCarte (className, nomLabel, bookPropertie) {
+  const elem = document.createElement("div");
+  elem.className = className;
+    const elemLabel = document.createElement("h1");
+    elemLabel.textContent = nomLabel;
+    const elemInput = document.createElement("div")
+    elemInput.className = "carteInput";
+    elemInput.textContent = bookPropertie;
+  elem.appendChild(elemLabel);
+  elem.appendChild(elemInput);
+  return elem;
 }
 
 function creationCarte (nodeParent) {
+
+  /*##################
+  #     DIV INFO     #
+  ##################*/
+
+  const nodeInfo = document.createElement("div")
+  nodeInfo.className = "livreInfoCarte";
    // TITRE DIV
-  const titreCarte = creationDivCarte("titreCarte", "Titre", book.titre);
+  const titreCarte = creerSectionCarte("titreCarte", "Titre", book.titre);
   // AUTHEUR DIV
-  const autheurCarte = creationDivCarte("autheurCarte", "Autheur", book.autheur);
+  const autheurCarte = creerSectionCarte("autheurCarte", "Autheur", book.autheur);
   // ANNEE PUBLICATION DIV
-  const releaseCarte = creationDivCarte("releaseCarte", "Année publication", book.release);
+  const releaseCarte = creerSectionCarte("releaseCarte", "Année publication", book.release);
   // GENRE DIV
-  const genreCarte = creationDivCarte("genreCarte", "Genre", book.genre);
+  const genreCarte = creerSectionCarte("genreCarte", "Genre", book.genre);
+
+  nodeInfo.appendChild(titreCarte);
+  nodeInfo.appendChild(autheurCarte);
+  nodeInfo.appendChild(releaseCarte);
+  nodeInfo.appendChild(genreCarte);
+  
+  /*-------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------*/
+
+  /*####################
+  # DIV BOUTON DU BAS  #
+  ####################*/
+
+  const nodeBouton = document.createElement("div")
+  nodeBouton.className = "livreBoutonCarte";
+
+  // Bouton supprimer
+  const bouton = document.createElement("button");
+  bouton.setAttribute("type", "button")
+  bouton.className = "buttonInput"
+  bouton.textContent = "Delete"
+
+  // checked boxe
+  const divCheckbox = document.createElement('div');
+  divCheckbox.className = "checkBox"
+  
+  const checkbox= document.createElement('input');
+  checkbox.type = "checkbox";
+  checkbox.name = "name";
+  checkbox.value = "value";
+  checkbox.id = "id";
+  let label = document.createElement('label');
+  label.htmlFor = "id";
+  label.appendChild(document.createTextNode('Read:'));
+  divCheckbox.appendChild(checkbox);
+  divCheckbox.appendChild(label);
+
+  nodeBouton.appendChild(bouton);
+  nodeBouton.appendChild(divCheckbox);
+/*-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------*/
+
+  /*####################
+  # DIV LIVRE COVER    #
+  ####################*/
+
+  const nodeLivreCover = document.createElement('div');
+  nodeLivreCover.className = "livreCover"
+
+/*-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------*/
+  
   // CARTE FINALE
-  const node = document.createElement("div")
+  const node = document.createElement("div");
   node.className = "livreCarte";
-  node.appendChild(titreCarte);
-  node.appendChild(autheurCarte);
-  node.appendChild(releaseCarte);
-  node.appendChild(genreCarte);
+  node.appendChild(nodeInfo);
+  node.appendChild(nodeBouton);
+  node.appendChild(nodeLivreCover);
   node.setAttribute("id", book.id);
+  // Ajout de la carte sur l'affichage.
   nodeParent.appendChild(node);
 }
 
@@ -68,9 +127,27 @@ function afficherLivre() {
       else {
       // Si le livre n'est pas affiché, on créé sa div et on l'affiche dans main.
         creationCarte(main);
+        // Supprimer le livre du DOM et de myLibrary si l'utilisateur appuie sur le bouton "Delete" du livre.
+        deleteButton = document.querySelectorAll('.buttonInput');
+        deleteButton.forEach(doc => doc.addEventListener('click', (event) => 
+        {
+          let livre = event.target.parentNode
+          const index = myLibrary.findIndex(library => library.id == livre.id)
+          myLibrary.splice(index, 1);
+          console.log(myLibrary);
+          event.target.parentNode.parentNode.remove();
+        }))
+
       }
     };
 };
+
+/* let deleteButton = document.querySelectorAll('.buttonInput');
+deleteButton.forEach((doc) => doc.addEventListener('click', () => {
+console.log("parent");
+})); */
+
+afficherLivre();
 
 // Détection d'ajout d'un livre par l'utilisateur.
 const formButton = document.getElementById('form_button');
